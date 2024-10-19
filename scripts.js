690d01db7a7575c58df994cf52466df356aa6bd3
DounarDaniel <dounardaniel@gmail.com>
2024-10-19 17:45:48 +0300

    Task_3

--- scripts.js
+++ scripts.js
@@ -5,12 +5,24 @@
 //     alert("Нечётное")
 // }
 
-let num1 = prompt("Введите первое число");
-let num2 = prompt("Введите второе число");
-if (num1 === num2){
-    alert("Равны");
-} else if(num1 > num2){
-    alert("Первое число больше второго");
+
+// let num1 = prompt("Введите первое число");
+// let num2 = prompt("Введите второе число");
+// if (num1 === num2){
+//     alert("Равны");
+// } else if(num1 > num2){
+//     alert("Первое число больше второго");
+// } else {
+//     alert("Второе число больше первого");
+// }
+
+
+let login = "Boss";
+let password = "12345"
+let login_1 = prompt("Введите логин:");
+let password_1 = prompt("Введите пароль:");
+if(login == login_1, password == password_1){
+    alert("Доступ разрешён");
 } else {
-    alert("Второе число больше первого");
+    alert("Доступ запрещён");
 }
