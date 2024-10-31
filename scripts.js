16ed27545654307de560ee542a746c0c63a6687b
DounarDaniel <dounardaniel@gmail.com>
2024-10-31 17:12:22 +0300

    Task_3

--- scripts.js
+++ scripts.js
@@ -6,11 +6,30 @@
 // delete user.name;
 // alert(user.name);
 
-function isEmpty(object){
-    for(key in object){
-        if(object.hasOwnProperty(key)){
-            return false;
+
+// function isEmpty(object){
+//     for(key in object){
+//         if(object.hasOwnProperty(key)){
+//             return false;
+//     }
+// }
+// return true;
+// }
+
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+};
+function SumSalaries(salaries){
+    sum = 0;
+    for(let person in salaries){
+        if(salaries.hasOwnProperty(person)){
+            sum += salaries[person];
     }
 }
-return true;
+return sum;
 }
+let totalSalaries = SumSalaries(salaries);
+alert(totalSalaries);
