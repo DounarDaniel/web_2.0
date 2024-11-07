9fe4faf029e0dbcc1e9138b0ef0a909921dacefa
DounarDaniel <dounardaniel@gmail.com>
2024-11-07 19:10:46 +0300

    Task_1

--- Clone_Objects/scripts.js
+++ Clone_Objects/scripts.js
@@ -1,12 +1,13 @@
-const original = {
-    a: 1,
-    b: {
-        c: 2
+const rectangle = {
+    width: 4,
+    height: 5,
+    getArea() {
+      return this.width * this.height;
+    },
+    getPerimeter() {
+      return 2 * (this.width + this.height);
     }
-};
-for(let key in original){
-    clone[key] = original[key];
-};
-console.log(clone);
-clone.b = 3;
-console.log(original);
+  };
+  
+  console.log(rectangle.getArea());
+  console.log(rectangle.getPerimeter());
