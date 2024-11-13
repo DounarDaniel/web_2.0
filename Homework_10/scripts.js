3306501e9a0f503fc586e0b553654088039b9c93
DounarDaniel <dounardaniel@gmail.com>
2024-11-13 21:14:09 +0300

    Task_1, Homework_10

---
+++ Homework_10/scripts.js
@@ -0,0 +1,24 @@
+let arr = [1,2,3,4,5]; // Task_1
+
+let arr2 = ["apple", "banana", "orange"]; // Task_2
+
+let arr3 = [
+    {name: "John"},
+    {name: "Alice"},
+    {name: "Bob"}
+]; // Task_3
+
+let arr4 = Array.of("hello", "world", "javascript"); // Task_4
+
+let arr5 = [1,2,3,4,5];
+let firstElement = arr5[2]; // Task_5
+
+let arr6 = [1,2,3,4,5];
+let firstElement = arr6[0];
+let firstElement1 = arr.shift();
+let firstElement = arr6.at(0); // Task_6
+
+let arr7 = [1,2,3,4,5];
+let lastElement = arr7[arr7.length - 1];
+let lastElement = arr7.at(-1);
+let lastElement = arr7.pop(); // Task_7
