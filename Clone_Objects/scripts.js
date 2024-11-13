4d75a40079a713cc76d34d5640e7fba9d6e082b4
DounarDaniel <dounardaniel@gmail.com>
2024-11-13 20:44:25 +0300

    Task_2 last version

--- Clone_Objects/scripts.js
+++ Clone_Objects/scripts.js
@@ -1,20 +1,8 @@
-const account = {
-    balance: 0,
-    deposit(amount) {
-      this.balance += amount;
-    },
-    withdraw(amount) {
-      if (amount > this.balance) {
-        console.log("Недостаточно средств");
-      } else {
-        this.balance -= amount;
-      }
-    },
-    getBalance() {
-      return this.balance;
-    }
-  };
-  account.deposit(100);
-  console.log(account.getBalance());
-  account.withdraw(50);
-  console.log(account.getBalance());
+person = {
+    name: Daniel,
+    age: 15,
+};
+greet(){
+    alert("Hello",  this.name);
+}
+greet();
