add5d1141cf22aa99e35fa32d856672f0131c86f
DounarDaniel <dounardaniel@gmail.com>
2024-11-14 22:24:16 +0300

    Task 3, Homework 10, part_2

--- Homework_10/scripts.js
+++ Homework_10/scripts.js
@@ -23,25 +23,39 @@
 // let lastElement = arr7.at(-1);
 // let lastElement = arr7.pop(); // Task_7
 
-let sum = [1,2,3,5];
-let sumOfNumbers = 0;
-for(let i = 0; i < sum.length; i++){
-    sumOfNumbers += sum[i];
-}
-console.log(sumOfNumbers); //Task 1, Homework 10, part_2
+// let sum = [1,2,3,5];
+// let sumOfNumbers = 0;
+// for(let i = 0; i < sum.length; i++){
+//     sumOfNumbers += sum[i];
+// }
+// console.log(sumOfNumbers); //Task 1, Homework 10, part_2
 
-function sumArrayWithValidation(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-     return ("Массив пуст или не является массивом");
-    }
-    let sum = 0;
-    for (const number of numbers) {
-     if (typeof number !== 'number') {
-      return ("Элемент не является числом");
-     }
-     sum += number;
-    }
-    return sum;
+// function sumArrayWithValidation(numbers) {
+//     if (!Array.isArray(numbers) || numbers.length === 0) {
+//      return ("Массив пуст или не является массивом");
+//     }
+//     let sum = 0;
+//     for (const number of numbers) {
+//      if (typeof number !== 'number') {
+//       return ("Элемент не является числом");
+//      }
+//      sum += number;
+//     }
+//     return sum;
+// }
+// const validNumbers = [1,2,3,4,5,6];
+// console.log(sumArrayWithValidation(validNumbers)); // Task 2, Homework 10, part_2
+
+let people = [
+    { name: 'John', age: 25, address: '123 Main St'},
+    { name: 'Jane', age: 17, address: '456 Elm St'},
+    { name: 'Bob', age: 30, address: '789 Oak St'},
+    { name: 'Alice', age: 22, address: 123},
+    { name: 'Mike', age: 35, address: '901 Maple St'}
+];
+function NamesOfAdults(people){
+    const result = people.filter(person => person.age >= 18 && typeof person.address === 'string').map(person => person.name);
+    return result;
+
 }
-const validNumbers = [1,2,3,4,5,6];
-console.log(sumArrayWithValidation(validNumbers)); // Task 2, Homework 10, part_2
+console.log(NamesOfAdults(people)); // Task 3, Homework 10, part_2
