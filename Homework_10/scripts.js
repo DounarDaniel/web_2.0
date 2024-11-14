6c1525a9f6ac228645b9d30b01ad373fc8c0121f
DounarDaniel <dounardaniel@gmail.com>
2024-11-14 20:42:25 +0300

    Task_2,Homework_10,part_2

--- Homework_10/scripts.js
+++ Homework_10/scripts.js
@@ -29,3 +29,19 @@ for(let i = 0; i < sum.length; i++){
     sumOfNumbers += sum[i];
 }
 console.log(sumOfNumbers); //Task 1, Homework 10, part_2
+
+function sumArrayWithValidation(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+     return ("Массив пуст или не является массивом");
+    }
+    let sum = 0;
+    for (const number of numbers) {
+     if (typeof number !== 'number') {
+      return ("Элемент не является числом");
+     }
+     sum += number;
+    }
+    return sum;
+}
+const validNumbers = [1,2,3,4,5,6];
+console.log(sumArrayWithValidation(validNumbers)); // Task 2, Homework 10, part_2
