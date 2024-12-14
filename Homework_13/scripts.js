d1d5c1963b4c3a3d1ea0d9e62e67df6825eea102
DounarDaniel <dounardaniel@gmail.com>
2024-12-14 14:54:32 +0300

    Lesson_Tasks

---
+++ Homework_13/scripts.js
@@ -0,0 +1,34 @@
+// let name = "John";
+// function sayHi() {
+// alert("Hi, " + name);
+// }
+// name = "Pete";
+// sayHi(); // будет показано Pete; Task_1
+
+
+// let phrase = "Hello";
+// if (true) {
+// let user = "John";
+// function sayHi() {
+// alert(`${phrase}, ${user}`);
+// }
+// }
+// sayHi(); // Hello, John; Task_2
+
+
+// function makeCounter() {
+//     let count = 0;
+//     return function() {
+//     return count++;
+//     };
+//     }
+//     let counter = makeCounter();
+//     let counter2 = makeCounter();
+//     alert( counter() ); // 0
+//     alert( counter() ); // 1
+//     alert( counter2() ); // 0
+//     alert( counter2() ); // 1; Task_3
+
+
+// const sum = a => b => a + b;
+// console.log(sum(1)(2)) // Task_4
